cf20ee96166eab9a536f7f6d7da937ef3ed436b9
GunaRakulan-GR <gunarakulan@gmail.com>
2023-12-01 02:35:54 +0530

    docs: update readme

--- soruce/main.py
+++ soruce/main.py
@@ -1,78 +1,90 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
-import cv2  # Install opencv-python
+import cv2
 import numpy as np
 from pyfirmata import Arduino, util
-import time
 
-# Define the serial port (check your Arduino IDE for the correct port)
-port = 'COM3'  # Change 'COM3' to your Arduino's serial port
+board = Arduino('COM4') # Change 'COM4' to your Arduino's serial port
 
-# Create a new Arduino board instance
-board = Arduino(port)
+red_pin = board.get_pin('d:13:o')     # Digital output pin 13
+green_pin = board.get_pin('d:12:o')   # Digital output pin 12
+blue_pin = board.get_pin('d:11:o')    # Digital output pin 11
 
-# Define the LED pin (e.g., pin 13)
-red_pin = board.get_pin('d:13:o')  # Digital output pin 13
-green_pin = board.get_pin('d:12:o')  # Digital output pin 12
+def get_dominant_color(frame, threshold=500):
+    # Convert the BGR image to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-# Disable scientific notation for clarity
-np.set_printoptions(suppress=True)
+    # Define color ranges in HSV
+    lower_red = np.array([0, 100, 100])
+    upper_red = np.array([10, 255, 255])
 
-# Load the model
-model = load_model("model/keras_Model.h5", compile=False)
+    lower_green = np.array([40, 40, 40])
+    upper_green = np.array([80, 255, 255])
 
-# Load the labels
-class_names = open("model/labels.txt", "r").readlines()
+    lower_blue = np.array([100, 100, 100])
+    upper_blue = np.array([140, 255, 255])
 
-# CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(0)
+    # Threshold the image to get only specified colors
+    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+    mask_green = cv2.inRange(hsv, lower_green, upper_green)
+    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Get the area of each color
+    red_area = cv2.countNonZero(mask_red)
+    green_area = cv2.countNonZero(mask_green)
+    blue_area = cv2.countNonZero(mask_blue)
+
+    # Determine the dominant color
+    dominant_color = "Unknown"
+    max_area = max(red_area, green_area, blue_area)
+    
+    if max_area > threshold:
+        if max_area == red_area:
+            dominant_color = "Red"
+        elif max_area == green_area:
+            dominant_color = "Green"
+        elif max_area == blue_area:
+            dominant_color = "Blue"
+
+    return dominant_color
+
+# Open the camera
+cap = cv2.VideoCapture(0)
 
 while True:
-    # Grab the webcamera's image.
-    ret, image = camera.read()
+    # Read a frame from the camera
+    ret, frame = cap.read()
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
-
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-
-    # Normalize the image array
-    image = (image / 127.5) - 1
-
-    # Predicts the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
-
-    # Print prediction and confidence score
-    #print("Class:", class_name[2:], end="")
-    #print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
-
-    if class_name[2:].strip() == "red":
-        red_pin.write(1)
-        green_pin.write(0)
-
-    elif class_name[2:].strip() == "green":
-        red_pin.write(0)
-        green_pin.write(1)
-
-    elif class_name[2:].strip() == "background":
-        red_pin.write(0)
-        green_pin.write(0)
-
-
-    # Listen to the keyboard for presses.
-    keyboard_input = cv2.waitKey(1)
-
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
+    if not ret:
+        print("Error reading frame")
         break
 
-board.exit()
-camera.release()
+    # Get the dominant color in the frame
+    dominant_color = get_dominant_color(frame)
+
+    # Draw the dominant color on the frame
+    cv2.putText(frame, f"Dominant Color: {dominant_color}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+
+    if dominant_color == "Red":
+        red_pin.write(1)
+
+    elif dominant_color == "Green":
+        green_pin.write(1)
+
+    elif dominant_color == "Blue":
+        blue_pin.write(1)
+    else:
+        red_pin.write(0)
+        green_pin.write(0)
+        blue_pin.write(0)
+
+
+    # Display the annotated frame
+    cv2.imshow('Dominant Color Detection', frame)
+
+    # Break the loop when 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all windows
+cap.release()
 cv2.destroyAllWindows()
